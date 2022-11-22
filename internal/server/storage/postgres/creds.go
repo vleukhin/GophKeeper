26fb48d29a4cd5544e2c29bacdd4f423ba12bee9
vleukhin <vsleuhin@ya.ru>
2022-11-22 11:06:07 +0700

    feat: implement server creds and notes storage

--- internal/server/storage/postgres/creds.go
+++ internal/server/storage/postgres/creds.go
@@ -3,30 +3,84 @@ package postgres
 import (
 	"context"
 	"github.com/google/uuid"
+	"github.com/vleukhin/GophKeeper/internal/helpers/errs"
 	"github.com/vleukhin/GophKeeper/internal/models"
 )
 
+const getCredsQuery = `
+	SELECT id, name, login, password, meta from creds where user_id = $1
+`
+
 func (p Storage) GetCreds(ctx context.Context, user models.User) ([]models.Cred, error) {
-	//TODO implement me
-	panic("implement me")
+	var creds []models.Cred
+	rows, err := p.conn.Query(ctx, getCredsQuery, user.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var c models.Cred
+		err := rows.Scan(&c.ID, &c.Name, &c.Login, &c.Password, &c.Meta)
+		if err != nil {
+			return nil, err
+		}
+
+		creds = append(creds, c)
+	}
+
+	return creds, nil
 }
 
-func (p Storage) AddCred(ctx context.Context, login *models.Cred, userID uuid.UUID) error {
-	//TODO implement me
-	panic("implement me")
+const createCredQuery = `
+	INSERT INTO creds (id, user_id, name, login, password, meta)
+	VALUES ($1, $2, $3, $4, $5, $6) 
+`
+
+func (p Storage) AddCred(ctx context.Context, cred *models.Cred, userID uuid.UUID) error {
+	_, err := p.conn.Exec(ctx, createCredQuery, cred.ID, userID, cred.Name, cred.Login, cred.Password, cred.Meta)
+	return err
 }
 
-func (p Storage) DelCred(ctx context.Context, loginID, userID uuid.UUID) error {
-	//TODO implement me
-	panic("implement me")
+const delCredQuery = `
+	DELETE FROM creds WHERE id = $1
+`
+
+func (p Storage) DelCred(ctx context.Context, credID, userID uuid.UUID) error {
+	if !p.IsCardOwner(ctx, credID, userID) {
+		return errs.ErrWrongOwnerOrNotFound
+	}
+	_, err := p.conn.Exec(ctx, delCredQuery, credID)
+	return err
 }
 
-func (p Storage) UpdateCred(ctx context.Context, login *models.Cred, userID uuid.UUID) error {
-	//TODO implement me
-	panic("implement me")
+const updateCredQuery = `
+	UPDATE creds SET
+		 name = $1, 
+		 login = $2,
+		 password = $3, 
+		 meta = $4
+	WHERE id = $5
+`
+
+func (p Storage) UpdateCred(ctx context.Context, cred *models.Cred, userID uuid.UUID) error {
+	if !p.IsCredOwner(ctx, cred.ID, userID) {
+		return errs.ErrWrongOwnerOrNotFound
+	}
+	_, err := p.conn.Exec(ctx, updateCredQuery,
+		cred.Name,
+		cred.Login,
+		cred.Password,
+		cred.Meta,
+		userID,
+	)
+
+	return err
 }
 
-func (p Storage) IsCredOwner(ctx context.Context, loginID, userID uuid.UUID) bool {
-	//TODO implement me
-	panic("implement me")
+const getCredByID = `
+	SELECT id FROM creds WHERE id = $1 and user_id = $2
+`
+
+func (p Storage) IsCredOwner(ctx context.Context, credID, userID uuid.UUID) bool {
+	return p.conn.QueryRow(ctx, getCredByID, credID, userID).Scan() == nil
 }
--- internal/server/storage/postgres/notes.go
+++ internal/server/storage/postgres/notes.go
@@ -3,30 +3,82 @@ package postgres
 import (
 	"context"
 	"github.com/google/uuid"
+	"github.com/vleukhin/GophKeeper/internal/helpers/errs"
 	"github.com/vleukhin/GophKeeper/internal/models"
 )
 
+const getNotesQuery = `
+	SELECT id, name, text, meta from notes where user_id = $1
+`
+
 func (p Storage) GetNotes(ctx context.Context, user models.User) ([]models.Note, error) {
-	//TODO implement me
-	panic("implement me")
+	var notes []models.Note
+	rows, err := p.conn.Query(ctx, getNotesQuery, user.ID)
+	if err != nil {
+		return nil, err
+	}
+
+	for rows.Next() {
+		var c models.Note
+		err := rows.Scan(&c.ID, &c.Name, &c.Text, &c.Meta)
+		if err != nil {
+			return nil, err
+		}
+
+		notes = append(notes, c)
+	}
+
+	return notes, nil
 }
+
+const createNoteQuery = `
+	INSERT INTO notes (id, user_id, name, text, meta)
+	VALUES ($1, $2, $3, $4, $5) 
+`
 
 func (p Storage) AddNote(ctx context.Context, note *models.Note, userID uuid.UUID) error {
-	//TODO implement me
-	panic("implement me")
+	_, err := p.conn.Exec(ctx, createNoteQuery, note.ID, userID, note.Name, note.Text, note.Meta)
+	return err
 }
+
+const delNoteQuery = `
+	DELETE FROM notes WHERE id = $1
+`
 
 func (p Storage) DelNote(ctx context.Context, noteID, userID uuid.UUID) error {
-	//TODO implement me
-	panic("implement me")
+	if !p.IsCardOwner(ctx, noteID, userID) {
+		return errs.ErrWrongOwnerOrNotFound
+	}
+	_, err := p.conn.Exec(ctx, delNoteQuery, noteID)
+	return err
 }
+
+const updateNoteQuery = `
+	UPDATE notes SET
+		 name = $1, 
+		 text = $2,
+		 meta = $4
+	WHERE id = $5
+`
 
 func (p Storage) UpdateNote(ctx context.Context, note *models.Note, userID uuid.UUID) error {
-	//TODO implement me
-	panic("implement me")
+	if !p.IsNoteOwner(ctx, note.ID, userID) {
+		return errs.ErrWrongOwnerOrNotFound
+	}
+	_, err := p.conn.Exec(ctx, updateNoteQuery,
+		note.Name,
+		note.Text,
+		note.Meta,
+		userID,
+	)
+
+	return err
 }
 
+const getNoteByID = `
+	SELECT id FROM notes WHERE id = $1 and user_id = $2
+`
+
 func (p Storage) IsNoteOwner(ctx context.Context, noteID, userID uuid.UUID) bool {
-	//TODO implement me
-	panic("implement me")
+	return p.conn.QueryRow(ctx, getNoteByID, noteID, userID).Scan() == nil
 }
